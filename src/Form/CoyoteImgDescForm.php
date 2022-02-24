ed7cc600a10066768853cd7f1b8d81c8f2102fc9
Job van Achterberg <mail@3ode.nl>
2022-02-24 12:34:06 +0100

    Require dependencies when using module settings

--- src/Form/CoyoteImgDescForm.php
+++ src/Form/CoyoteImgDescForm.php
@@ -2,6 +2,11 @@
 
 namespace Drupal\coyote_img_desc\Form;
 
+require_once( __DIR__ . '/../../vendor/autoload.php');
+
+use Coyote\HelperFunctions;
+use Coyote\Model\OrganizationModel;
+use Coyote\Request\GetMembershipsRequest;
 use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
 
@@ -20,18 +25,33 @@ class CoyoteImgDescForm extends ConfigFormBase {
     $form = parent::buildForm($form, $form_state);
     $config = $this->config('coyote_img_desc.settings');
 
+    $token = $config->get('api_token');
+    $endpoint = $config->get('api_endpoint');
+
+    $suffix = '';
+
+    if (isset($token) && isset($endpoint)) {
+      $profile = HelperFunctions::getProfile($endpoint, $token);
+      if (is_null($profile)) {
+        $suffix = 'Unable to load a valid API profile.';
+      } else {
+        $suffix = "User: {$profile->getName()}";
+      }
+    }
+
     $form['api_token'] = [
       '#type' => 'textfield',
       '#title' => $this->t('Coyote API token:'),
       '#default_value' => $config->get('api_token'),
       '#description' => $this->t('The API token associated with your account.'),
+      '#field_suffix' => $suffix
     ];
 
     $form['api_endpoint'] = [
       '#type' => 'textfield',
-      '#title' => $this->t('The endpoint for the Coyote API:'),
+      '#title' => $this->t('Coyote API endpoint:'),
       '#default_value' => $config->get('api_endpoint'),
-      '#description' => $this->t('For example, "https://live.coyote.pics/api/v1/"'),
+      '#description' => $this->t('For example: "https://live.coyote.pics/api/v1/"'),
     ];
 
     return $form;
@@ -40,9 +60,50 @@ class CoyoteImgDescForm extends ConfigFormBase {
   /**
    * {@inheritdoc}
    */
-  protected function getEditableConfigNames() {
+  protected function getEditableConfigNames(): array {
     return [
       'coyote_img_desc.settings',
     ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    $endpoint = $form_state->getValue('api_endpoint');
+    $token = $form_state->getValue('api_token');
+    $profile = HelperFunctions::getProfile($endpoint, $token);
+
+    if (is_null($profile)) {
+      $form_state->setErrorByName('api_token', $this->t('The combination of token and/or endpoint is invalid.'));
+    }
+
+    $config = $this->config('coyote_img_desc.settings');
+    $organizations = HelperFunctions::getOrganizations($endpoint, $token);
+    $options = array_reduce($organizations, function(array $carry, OrganizationModel $item): array {
+      $carry[$item->getId()] = $item->getName();
+      return $carry;
+    }, []);
+
+    $form['api_organization'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Organization:'),
+      '#default_value' => $config->get('api_organization'),
+      '#options' => $options
+    ];
+
+    $form_state->setErrorByName('api_organization', $this->t('Select an organization.'));
+
+    $ddm = \Drupal::service('devel.dumper');
+    $ddm->debug([$profile, $endpoint, $token]);
+  }
+
+  public function submitForm(array &$form, FormStateInterface $form_state): void {
+    $config = $this->config('coyote_img_desc.settings');
+    $config->set('api_endpoint', $form_state->getValue('api_endpoint'));
+    $config->set('api_token', $form_state->getValue('api_token'));
+    $config->set('api_organization', $form_state->getValue('api_organization'));
+    $config->save();
+    parent::submitForm($form, $form_state);
   }
 }
